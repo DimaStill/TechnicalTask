38ad1707eea848054fc15daf69204f7c81f92c8f
DimaStill <dimastill123@gmail.com>
2019-09-23 20:33:41 +0300

    Initial commit

---
+++ TechnicalTask/Models/HumanName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalTask.Models
+{
+    public class HumanName
+    {
+        public string Text { get; set; }
+        public string Family { get; set; }
+        public string Given { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+    }
+}
---
+++ TechnicalTask/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalTask.Models
+{
+    public class Person
+    {
+        public string ResourceType { get; set; }
+        public HumanName Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Acive { get; set; }
+    }
+}
