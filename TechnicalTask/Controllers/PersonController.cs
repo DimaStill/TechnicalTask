07396ab14e644378ddc31f2f27636ff9fb9150de
DimaStill <dimastill123@gmail.com>
2019-09-24 17:14:49 +0300

    Create person controller

--- TechnicalTask/Controllers/PersonController.cs
+++ TechnicalTask/Controllers/PersonController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TechnicalTask.Models;
+using TechnicalTask.Services.Interfaces;
 
 namespace TechnicalTask.Controllers
 {
@@ -11,36 +13,44 @@ namespace TechnicalTask.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+        readonly IPersonService service;
+        public PersonController(IPersonService personService)
+        {
+            service = personService;
+        }
+
         // GET: api/Person
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<Person>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return await service.GetAllPeson();
         }
 
         // GET: api/Person/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public async Task<Person> Get(int id)
         {
-            return "value";
+            return await service.GetPerson(id);
         }
 
         // POST: api/Person
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<Person> Post([FromBody] Person newPerson)
         {
+            return await service.CreatePerson(newPerson);
         }
 
         // PUT: api/Person/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<Person> Put(int id, [FromBody] Person person)
         {
+            return await service.UpdatePerson(id, person);
         }
 
-        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<bool> Delete(int id)
         {
+            return await service.DeletePerson(id);
         }
     }
 }
