0a63724772411b9de9f7a27dc07c01a7e35558d2
DimaStill <dimastill123@gmail.com>
2019-09-26 14:47:34 +0300

    Update models

--- TechnicalTask/Models/HumanName.cs
+++ TechnicalTask/Models/HumanName.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TechnicalTask.Models
 {
     public class HumanName
     {
+        [JsonProperty(PropertyName = "text")]
         public string Text { get; set; }
+        [JsonProperty(PropertyName = "family")]
         public string Family { get; set; }
+        [JsonProperty(PropertyName = "given")]
         public List<string> Given { get; set; }
+        [JsonProperty(PropertyName = "prefix")]
         public List<string> Prefix { get; set; }
+        [JsonProperty(PropertyName = "suffix")]
         public List<string> Suffix { get; set; }
     }
 }
--- TechnicalTask/Models/Person.cs
+++ TechnicalTask/Models/Person.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TechnicalTask.Models
 {
     public class Person
     {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "resourceType")]
         public string ResourceType { get; set; }
+        [JsonProperty(PropertyName = "name")]
         public List<HumanName> Name { get; set; }
+        [JsonProperty(PropertyName = "birthDate")]
         public DateTime BirthDate { get; set; }
-        public bool Acive { get; set; }
+        [JsonProperty(PropertyName = "active")]
+        public bool Active { get; set; }
     }
 }
